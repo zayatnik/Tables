69149937710808cb0b2b1ee6afc6be00015b06e7
zayatnik <55553752+zayatnik@users.noreply.github.com>
2020-03-30 14:53:38 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,1 +1,24 @@
+#include "Polynom.h"
 
+#pragma once
+
+struct elem {
+	Polynom val;
+	int key;
+};
+
+class Table{
+public:
+	virtual bool isempty() {
+	}
+	virtual bool isfull() {
+	}
+	virtual int size() {
+	}
+	virtual void insert(Polynom x) {
+	}
+	virtual void remove(int key1) {
+	}
+	virtual elem search(int key1) {
+	}
+};
